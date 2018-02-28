30ebc44a6be9ee7a6070fe833340ad4cd2fe6ca1
baolijn <libao_fighting@163.com>
2018-02-28 11:22:24 +0800

    rest api json

--- src/main/java/com/libao/miaosha/controller/DemoController.java
+++ src/main/java/com/libao/miaosha/controller/DemoController.java
@@ -1,12 +1,33 @@
 package com.libao.miaosha.controller;
 
+import com.libao.miaosha.result.CodeMsg;
+import com.libao.miaosha.result.Result;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/demo")
 public class DemoController {
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    //1.rest api json输出
+    @RequestMapping("/hello")
+    @ResponseBody
+    public Result<String> hello() {
+        return Result.success("hello");
+    }
+    @RequestMapping("/helloError")
+    @ResponseBody
+    public Result<String> helloError() {
+        return Result.error(CodeMsg.SERVER_ERROR);
+        //return new Result(500102, "XXX");
+    }
     @RequestMapping("/thymeleaf")
     public String  thymeleaf(Model model) {
         model.addAttribute("name", "Libao");
---
+++ src/main/java/com/libao/miaosha/result/Result.java
@@ -0,0 +1,50 @@
+package com.libao.miaosha.result;
+
+public class Result<T> {
+    private int code;
+    private String msg;
+    private T data;
+
+    private Result(T data) {
+        this.data = data;
+    }
+    private Result(CodeMsg cm) {
+        if(cm == null) {
+            return;
+        }
+        this.code = cm.getCode();
+        this.msg = cm.getMsg();
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public static<T> Result<T> success(T data){
+        return new Result<T>(data);
+    }
+
+    public static <T> Result<T> error(CodeMsg cm){
+        return new  Result<T>(cm);
+    }
+}
